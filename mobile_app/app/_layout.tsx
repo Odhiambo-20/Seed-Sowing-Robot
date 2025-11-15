be3de6671686fd3f01a2c925daa3f19367324183
Victor Odhiambo <odhiambov110@gmail.com>
2025-11-16 01:48:09 +0300

    created _layout.tsx

---
+++ mobile_app/app/_layout.tsx
@@ -0,0 +1,39 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import React, { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { AuthProvider } from "@/providers/AuthProvider";
+import { RobotProvider } from "@/providers/RobotProvider";
+
+SplashScreen.preventAutoHideAsync();
+
+const queryClient = new QueryClient();
+
+function RootLayoutNav() {
+  return (
+    <Stack screenOptions={{ headerBackTitle: "Back", headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
+
+export default function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <AuthProvider>
+          <RobotProvider>
+            <RootLayoutNav />
+          </RobotProvider>
+        </AuthProvider>
+      </GestureHandlerRootView>
+    </QueryClientProvider>
+  );
+}
