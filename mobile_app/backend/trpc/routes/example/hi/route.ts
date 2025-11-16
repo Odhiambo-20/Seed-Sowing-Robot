8c8cc054bdc5ca926fe395e858b4ce00c9ba8931
Victor Odhiambo <odhiambov110@gmail.com>
2025-11-16 04:10:19 +0300

    updated create-context.ts

--- mobile_app/backend/trpc/routes/example/hi/route.ts
+++ mobile_app/backend/trpc/routes/example/hi/route.ts
@@ -1,11 +1,7 @@
-import { z } from "zod";
-import { publicProcedure } from "@/backend/trpc/create-context";
 
-export default publicProcedure
-  .input(z.object({ name: z.string() }))
-  .mutation(({ input }) => {
-    return {
-      hello: input.name,
-      date: new Date(),
-    };
-  });
+import { createTRPCRouter } from "../../../create-context";
+
+
+
+export default createTRPCRouter({});
+
